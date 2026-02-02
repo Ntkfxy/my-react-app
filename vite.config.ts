10fff8232b944ab9c089139d56186213cb8aafe2
Ntkfxy <natthaan57101@gmail.com>
2026-02-02 10:27:20 +0700

    commit deploy build react-app

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-   base: '/react-app/',
+   base: '/my-react-app/',
    plugins: [react()],
   resolve: {
     alias: {
